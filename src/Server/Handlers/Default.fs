d0469c4023a0f176e2d011464022aa4d2690d627
Onur Gumus <onur@outlook.com.tr>
2023-07-17 12:35:05 +0000

    add default handler

---
+++ src/Server/Handlers/Default.fs
@@ -0,0 +1,25 @@
+module FunPizzaShop.Server.Handlers.Default
+open System.Threading.Tasks
+open Giraffe
+open Microsoft.AspNetCore.Http
+open FunPizzaShop.Server.Views
+
+let webApp (env: #_) (layout: HttpContext -> (int -> Task<string>) -> string Task) =
+
+    let viewRoute view = 
+        fun (next: HttpFunc) (ctx: HttpContext) ->
+            task {
+                let! lay = (layout ctx (view ctx))
+                return! htmlString lay next ctx
+            }
+
+    let defaultRoute = viewRoute (Index.view env)
+
+    choose [ 
+        routeCi "/" >=> defaultRoute
+    ]
+
+let webAppWrapper (env: #_) (layout: HttpContext -> (int -> Task<string>) -> string Task) =
+    fun (next: HttpFunc) (context: HttpContext) -> task { 
+        return! webApp env layout next context
+     }
