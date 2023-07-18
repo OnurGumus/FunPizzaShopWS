b423ac07231bae41d5ac18ee79919b8889c4fc03
Onur Gumus <onur@outlook.com.tr>
2023-07-18 11:02:42 +0000

    Add Shared Interfaces

---
+++ src/ServerInterfaces/Query.fs
@@ -0,0 +1,9 @@
+﻿module FunPizzaShop.ServerInterfaces.Query
+open FunPizzaShop.Shared.Model
+open Akka.Streams.Dsl
+open Akka.Streams
+open FunPizzaShop.Shared.Model.Pizza
+
+[<Interface>]
+type IQuery =
+    abstract Query<'t> : ?filter:Predicate * ?orderby:string * ?orderbydesc:string * ?thenby:string  * ?thenbydesc:string * ?take:int * ?skip:int -> list<'t> Async
