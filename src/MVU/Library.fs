2a3f9ec4e1b825818d3eb436b06cf28391adec2e
Onur Gumus <onur@outlook.com.tr>
2023-07-18 12:07:44 +0000

    Add Pizza To MVU

--- src/MVU/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace MVU
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ src/MVU/PizzaItem.fs
@@ -0,0 +1,16 @@
+module FunPizzaShop.MVU.PizzaItem
+open Elmish
+open FunPizzaShop.Shared.Model.Pizza
+
+type Model = { PizzaSpecial: PizzaSpecial }
+
+type Msg = NA
+
+type Order = NoOrder
+
+    
+let init (pizzaSpecial:PizzaSpecial) () = 
+    {PizzaSpecial = pizzaSpecial} , NoOrder
+
+let update msg model =
+   model , NoOrder
