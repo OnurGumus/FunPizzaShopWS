0fd406678cefa4373ffc2c7ffa43e415f08244f3
Onur Gumus <onur@outlook.com.tr>
2023-07-18 12:31:44 +0000

    Work on pizza item ui

---
+++ src/Client/PizzaItem.fs
@@ -0,0 +1,20 @@
+module FunPizzaShop.Client.PizzaItem
+
+open Elmish
+open Elmish.HMR
+open Lit
+open Lit.Elmish
+open Browser.Types
+open Fable.Core.JsInterop
+open Fable.Core
+open System
+open Browser
+open Elmish.Debug
+open FsToolkit.ErrorHandling
+open ElmishOrder
+open Browser.Types
+open FunPizzaShop.MVU.PizzaItem
+open Thoth.Json
+open FunPizzaShop.Shared.Model.Pizza
+open FunPizzaShop.Shared.Model
+open FunPizzaShop.Shared.Constants
