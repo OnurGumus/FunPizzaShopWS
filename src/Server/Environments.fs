f9c734463a382a6a8621142ba4e1be93deb225b8
Onur Gumus <onur@outlook.com.tr>
2023-07-18 11:04:42 +0000

    add server interfaces to server

--- src/Server/Environments.fs
+++ src/Server/Environments.fs
@@ -3,10 +3,11 @@ module FunPizzaShop.Server.Environments
 
 open System
 open Microsoft.Extensions.Configuration
+open FunPizzaShop.ServerInterfaces.Query
 
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 type AppEnv(config: IConfiguration) =
-
+ 
     interface IConfiguration with
         member _.Item
             with get (key: string) = config.[key]
@@ -15,5 +16,10 @@ type AppEnv(config: IConfiguration) =
         member _.GetChildren() = config.GetChildren()
         member _.GetReloadToken() = config.GetReloadToken()
         member _.GetSection key = config.GetSection(key)
+
+
+    interface IQuery with
+        member _.Query(?filter, ?orderby,?orderbydesc, ?thenby, ?thenbydesc, ?take, ?skip) =
+            failwith "Not implemented"
 
     member _.Reset()  = ()
