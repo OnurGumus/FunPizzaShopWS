9d9ef927644af27c19ecb3af591ccf88aa8ba97e
Onur Gumus <onur@outlook.com.tr>
2023-07-18 11:53:27 +0000

    add mock data

--- src/Server/Environments.fs
+++ src/Server/Environments.fs
@@ -4,6 +4,8 @@ module FunPizzaShop.Server.Environments
 open System
 open Microsoft.Extensions.Configuration
 open FunPizzaShop.ServerInterfaces.Query
+open FunPizzaShop.Shared.Model.Pizza
+open FunPizzaShop.Shared.Model
 
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 type AppEnv(config: IConfiguration) =
@@ -19,7 +21,57 @@ type AppEnv(config: IConfiguration) =
 
 
     interface IQuery with
-        member _.Query(?filter, ?orderby,?orderbydesc, ?thenby, ?thenbydesc, ?take, ?skip) =
-            failwith "Not implemented"
+        member _.Query<'t>(?filter, ?orderby,?orderbydesc, ?thenby, ?thenbydesc, ?take, ?skip) =
+            let res = 
+                if typeof<'t> = typeof<PizzaSpecial> then
+                    let special1 : PizzaSpecial = {
+                        Id = 1L |> SpecialId.TryCreate |>forceValidate 
+                        Name = "Basic Cheese Pizza" |> ShortString.TryCreate |> forceValidate
+                        BasePrice = 9.99m |> Price.TryCreate |> forceValidate
+                        Description = "It's cheesy and delicious. Why wouldn't you want one?" |> ShortString.TryCreate |> forceValidate
+                        ImageUrl = "img/pizzas/cheese.jpg" |> ShortString.TryCreate |> forceValidate
+                    }
+                    let special2 : PizzaSpecial = {
+                        Id = 2L |> SpecialId.TryCreate |>forceValidate 
+                        Name = "The Baconatorizor" |> ShortString.TryCreate |> forceValidate
+                        BasePrice =11.99m |> Price.TryCreate |> forceValidate
+                        Description = "It has EVERY kind of bacon"|> ShortString.TryCreate |> forceValidate
+                        ImageUrl = "img/pizzas/bacon.jpg" |> ShortString.TryCreate |> forceValidate
+                    }
+                    let special3 : PizzaSpecial = {
+                        Id = 3L |> SpecialId.TryCreate |>forceValidate 
+                        Name = "Classic pepperoni" |> ShortString.TryCreate |> forceValidate
+                        BasePrice = 10.50m |> Price.TryCreate |> forceValidate
+                        Description = "It's the pizza you grew up with, but Blazing hot!" |> ShortString.TryCreate |> forceValidate
+                        ImageUrl = "img/pizzas/pepperoni.jpg" |> ShortString.TryCreate |> forceValidate
+                    }
+                    let special4 : PizzaSpecial = {
+                        Id = 4L |> SpecialId.TryCreate |>forceValidate 
+                        Name = "Buffalo chicken" |> ShortString.TryCreate |> forceValidate
+                        BasePrice = 12.75M |> Price.TryCreate |> forceValidate
+                        Description = "Spicy chicken, hot sauce and bleu cheese, guaranteed to warm you up" |> ShortString.TryCreate |> forceValidate
+                        ImageUrl = "img/pizzas/meaty.jpg"|> ShortString.TryCreate |> forceValidate
+                    }
+                    [special1; special2; special3; special4] 
+                    |> List.ofSeq
+                    |> box
+                elif typeof<'t> = typeof<Topping> then
+                    let topping : Topping = {
+                        Id = 1L |> ToppingId.TryCreate |>forceValidate 
+                        Name = "Mushrooms" |> ShortString.TryCreate |> forceValidate
+                        Price = 1m |> Price.TryCreate |> forceValidate
+                    }
+                    let topping : Topping = {
+                        Id = 2L |> ToppingId.TryCreate |>forceValidate 
+                        Name = "Duck sausage" |> ShortString.TryCreate |> forceValidate
+                        Price = 1m |> Price.TryCreate |> forceValidate
+                    }
+                    [topping] 
+                    |> List.ofSeq
+                    |> box
+               
+                else
+                    failwith "Not implemented"
+            async { return res :?> list<'t> }
 
     member _.Reset()  = ()
