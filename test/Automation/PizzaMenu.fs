47a8eac1d8173ca716f8a1b40ee22cb0e0b19fdb
Onur Gumus <onur@outlook.com.tr>
2023-07-18 09:22:55 +0000

    Add failing test

--- test/Automation/PizzaMenu.fs
+++ test/Automation/PizzaMenu.fs
@@ -7,8 +7,20 @@ open type Microsoft.Playwright.Assertions
 
 [<When>]
 let ``I get the main menu``  (context:IBrowserContext)= 
-        ()
+        (task {
+                let! page = context.NewPageAsync()
+                let! _ = page.GotoAsync("http://localhost:5010")
+                return page
+        }).Result
+        
 
 
 [<Then>]
-let ``pizza items should be fetched`` ()= ()
+let ``pizza items should be fetched`` (page:IPage)= 
+        (task{
+                let! pizzaItems = page.QuerySelectorAllAsync("fps-pizza-item")
+                if pizzaItems.Count = 0 then
+                        failwith "No pizza items found"
+                else
+                        return ()
+        }).Wait()
