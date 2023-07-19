c3b791e3e584c40354bc59505aea4ef88c87927f
Onur Gumus <onur@outlook.com.tr>
2023-07-19 11:31:26 +0000

    add login store

---
+++ src/MVU/LoginStore.fs
@@ -0,0 +1,22 @@
+module FunPizzaShop.MVU.LoginStore
+open ElmishStore
+open FunPizzaShop.Shared.Model.Authentication
+
+type Model = { UserId: UserId option }
+
+type Msg = 
+    | LoggedIn of UserId
+    | LoggedOut 
+
+[<RequireQualifiedAccessAttribute>]
+type Order =
+    | NoOrder
+
+let init () = { UserId = None }, Order.NoOrder
+
+let update (msg: Msg) (model: Model) =
+    match msg with
+    | LoggedIn userId -> { model with UserId = Some userId },Order.NoOrder
+    | LoggedOut -> { model with UserId = None },Order.NoOrder
+
+let dispose _ = ()
