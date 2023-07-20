cda0a8e87fdd55297a42492eb609538c5f01144d
Onur Gumus <onur@outlook.com.tr>
2023-07-20 10:42:02 +0000

    Add serializer

---
+++ src/Command/Domain/Serialization.fs
@@ -0,0 +1,75 @@
+module Command.Serialization
+
+open Command
+open Akkling
+open Akka.Actor
+open Akka.Serialization
+open System.Text
+open NodaTime
+open Thoth.Json.Net
+open System.Runtime.Serialization
+open Serilog
+open System
+open FunPizzaShop.Command.Domain
+
+module DefaultEncode =
+    let instant (instant: Instant) =
+        Encode.datetime (instant.ToDateTimeUtc())
+
+module DefeaultDecode =
+    let instant: Decoder<Instant> =
+        Decode.datetimeUtc |> Decode.map (Instant.FromDateTimeUtc)
+
+
+let extraThoth =
+    Extra.empty
+    |> Extra.withInt64
+    |> Extra.withDecimal
+    |> Extra.withCustom (DefaultEncode.instant) DefeaultDecode.instant
+
+//Event encoding
+let userMessageEncode =
+    Encode.Auto.generateEncoder<Common.Event<User.Event>> (extra = extraThoth)
+let userMessageDecode =
+    Decode.Auto.generateDecoder<Common.Event<User.Event>> (extra = extraThoth)
+
+
+
+/// State encoding
+let userStateEncode = Encode.Auto.generateEncoder<User.State> (extra = extraThoth)
+let userStateDecode = Decode.Auto.generateDecoder<User.State> (extra = extraThoth)
+
+
+type ThothSerializer(system: ExtendedActorSystem) =
+    inherit SerializerWithStringManifest(system)
+
+    override _.Identifier = 1712
+
+    override _.ToBinary(o) =
+        match o with
+        | :? Common.Event<User.Event> as mesg -> mesg |> userMessageEncode
+        | :? User.State as mesg -> mesg |> userStateEncode
+
+    override _.Manifest(o: obj) : string =
+        match o with
+        | :? Common.Event<User.Event> -> "UserMessage"
+        | :? User.State -> "UserState"
+
+     override _.FromBinary(bytes: byte[], manifest: string) : obj =
+        let decode decoder =
+            Encoding.UTF8.GetString(bytes)
+            |> Decode.fromString decoder
+            |> function
+                | Ok res -> res
+                | Error er -> raise (new SerializationException(er))
+
+            
+        match manifest with
+        | "UserState" -> upcast decode userStateDecode
+        | "UserMessage" -> upcast decode userMessageDecode
+
+        | _ ->
+        Log.Fatal("manifest {manifest} not found", manifest)
+        Environment.FailFast("shouldn't happen")
+        raise (new SerializationException())
+
