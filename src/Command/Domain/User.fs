8ce32dde53453d8fb6bd86ae5e4af0be91f601f5
Onur Gumus <onur@outlook.com.tr>
2023-07-20 09:20:08 +0000

    actor WIP 1

--- src/Command/Domain/User.fs
+++ src/Command/Domain/User.fs
@@ -16,3 +16,34 @@ open FunPizzaShop.Shared.Model
 open Akka.Logger.Serilog
 open Akka.Event
 open FunPizzaShop.ServerInterfaces.Command
+
+
+type Command =
+    | Login
+    | VefifyLogin of VerificationCode option
+
+type Event =
+    | LoginSucceeded of VerificationCode option
+    | LoginFailed
+    | VerificationFailed
+    | VerificationSucceeded
+
+type State = {
+    Verification: VerificationCode option
+    Version: int64
+} with
+    interface IDefaultTag
+
+
+let actorProp (env:_) toEvent (mediator: IActorRef<Publish>) (mailbox: Eventsourced<obj>) =
+    let config  = env :> IConfiguration
+    let log = mailbox.UntypedContext.GetLogger()
+    let mediatorS = retype mediator
+    let sendToSagaStarter = SagaStarter.toSendMessage mediatorS mailbox.Self
+    let rec set (state: State) =
+        actor {
+            let! msg = mailbox.Receive()
+            log.Debug("Message {MSG}, State: {@State}", box msg, state)
+
+        }
+    failwith "not implemented"
